c11e99bf06aa81874f45631b9131c7b647a4d383
Sergey059 <sergeiv-82@mail.ru>
2021-08-11 13:27:35 +0500

    Finished checkout page

--- app/js/Accordion.js
+++ app/js/Accordion.js
@@ -2,20 +2,20 @@ export default function(Accordion) {
 
 	const accordion = document.querySelector('.accordion')
 	const accordionItem = document.querySelectorAll('.accordion__item')
-	const accordionItemHeading = accordion.querySelector('.accordion__title')
+	const accordionItemHeading = document.querySelectorAll('.accordion__title')
 
-	for(let item of accordionItem) {
+	for(let item of accordionItemHeading) {
 		item.addEventListener('click', function() {
-			if(this.classList.contains('active')){
-				this.classList.remove('active')
+			const parent=this.parentElement
+			if(parent.classList.contains('active')){
+				parent.classList.remove('active')
 			} else{
 				for(let el of accordionItem){
 					el.classList.remove('active')
 				}
-				this.classList.add('active')
+				parent.classList.add('active')
 			}
 		})
-		
 	}
 	
 }
